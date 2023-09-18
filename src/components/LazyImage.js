42f73228fdf8413c9866cdc69c84abe924f9faea
prometheus-k <prometheus140524@gmail.com>
2023-09-18 23:20:57 +0900

    이미지 로딩 수정

---
+++ src/components/LazyImage.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+
+const LazyImage = ({ src }) => {
+    // state
+    const [isLoading, setIsLoading] = React.useState(false); // 실제 화면에 보여지고 있는지 여부를 확인
+
+    // ref
+    const imgRef = React.useRef(null); // 이미지 태그 요소
+    const observer = React.useRef(); // IntersectionObserver 변수
+
+    // useEffect
+    React.useEffect(() => {
+        observer.current = new IntersectionObserver(intersectionOberserver); // 인스턴스 생성
+        imgRef.current && observer.current.observe(imgRef.current); // 이미지 태그 관찰 시작
+    }, [])
+
+    // IntersectionObserver 설정
+    const intersectionOberserver = (entries, io) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) { // 관찰되고 있는 entry가 보여지게 된 다면
+                io.unobserve(entry.target); // 관찰 종료
+                setIsLoading(true); // 로딩 체크
+            }
+        })
+    }
+
+    return (
+        // 화면에 보여지기 전이라면 NoImage, 화면에 보여지고 있다면 src에 해당하는 이미지
+        <img ref={imgRef} src={src} />
+    )
+}
+
+export default LazyImage
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -16,6 +16,9 @@ import Box from '../components/Box';
 import CardAction from '../components/CardAction';
 import Loading from '../components/Loading';
 import ErrorBoundary from './ErrorBoundary';
+import Image from "../components/Image";
+import LazyImage from '../components/LazyImage';
+
 
 // Import Swiper styles
 import 'swiper/css';
@@ -25,7 +28,7 @@ import 'swiper/css/pagination';
 import './Main.css'
 import hero_img_mw from '../assets/images/hero_img_mw.png';
 import hero_img_pc from '../assets/images/hero_img-2.png';
-import foot_img from '../assets/images/main_team_join.jpg';
+
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -49,7 +52,7 @@ const Default = () => {
   return isDefault;
 }
 
-const Image = React.lazy(() => import('../components/Image'));
+
 const CardMedia = React.lazy(() => import('../components/CardMedia'));
 
 const override = {
@@ -75,27 +78,30 @@ const Main = () => {
   const scrollItems = useRef([]);
   let [loading, setLoading] = useState(true);
   let [color, setColor] = useState("#D199D0");
+  const [bigBanner, setBigBanner] = useState(Mobile() ? hero_img_mw : hero_img_pc);
+  const [bigBannerClass, setBigBannerClass] = useState(Mobile() ? "m-img" : "pc-img");
+  const [footerImg, setFooterImg] = useState(Mobile() ? 'images/main/mw/mw_main_team_join.jpg' : 'images/main/pc/main_team_join.jpg');
 
   const initialBenefitCard = [
     {
-      title: '유연한 자율 출퇴근', 
-      sub: '개인의 라이프 스타일에 맞게 유연한 업무시간으로 일과 삶의 밸런스를 조절할 수 있어요.', 
-      imageSrc: Mobile() ? 'images/benefit/mw/mw_benetfit_01.jpg': 'images/benefit/pc/pc_benetfit_01.jpg'    
+      title: '유연한 자율 출퇴근',
+      sub: '개인의 라이프 스타일에 맞게 유연한 업무시간으로 일과 삶의 밸런스를 조절할 수 있어요.',
+      imageSrc: Mobile() ? 'images/benefit/mw/mw_benetfit_01.jpg' : 'images/benefit/pc/pc_benetfit_01.jpg'
     },
     {
-      title: '유연한 자율 출퇴근', 
-      sub: '개인의 라이프 스타일에 맞게 유연한 업무시간으로 일과 삶의 밸런스를 조절할 수 있어요.', 
-      imageSrc: Mobile() ? 'images/benefit/mw/mw_benetfit_02.jpg': 'images/benefit/pc/pc_benetfit_02.jpg'    
+      title: '유연한 자율 출퇴근',
+      sub: '개인의 라이프 스타일에 맞게 유연한 업무시간으로 일과 삶의 밸런스를 조절할 수 있어요.',
+      imageSrc: Mobile() ? 'images/benefit/mw/mw_benetfit_02.jpg' : 'images/benefit/pc/pc_benetfit_02.jpg'
     },
     {
-      title: '유연한 자율 출퇴근', 
-      sub: '개인의 라이프 스타일에 맞게 유연한 업무시간으로 일과 삶의 밸런스를 조절할 수 있어요.', 
-      imageSrc: Mobile() ? 'images/benefit/mw/mw_benetfit_03.jpg': 'images/benefit/pc/pc_benetfit_03.jpg'    
+      title: '유연한 자율 출퇴근',
+      sub: '개인의 라이프 스타일에 맞게 유연한 업무시간으로 일과 삶의 밸런스를 조절할 수 있어요.',
+      imageSrc: Mobile() ? 'images/benefit/mw/mw_benetfit_03.jpg' : 'images/benefit/pc/pc_benetfit_03.jpg'
     },
     {
-      title: '유연한 자율 출퇴근', 
-      sub: '개인의 라이프 스타일에 맞게 유연한 업무시간으로 일과 삶의 밸런스를 조절할 수 있어요.', 
-      imageSrc: Mobile() ? 'images/benefit/mw/mw_benetfit_04.jpg': 'images/benefit/pc/pc_benetfit_04.jpg'    
+      title: '유연한 자율 출퇴근',
+      sub: '개인의 라이프 스타일에 맞게 유연한 업무시간으로 일과 삶의 밸런스를 조절할 수 있어요.',
+      imageSrc: Mobile() ? 'images/benefit/mw/mw_benetfit_04.jpg' : 'images/benefit/pc/pc_benetfit_04.jpg'
     },
   ]
 
@@ -162,170 +168,169 @@ const Main = () => {
   }, []);
   return (
     // <ErrorBoundary>
-      <Box boxClassName="content">
-        <div className="default_ani">
-          <Box boxClassName="inner">
-            <div className="titleWrap">
-              <p className="title1">돈을 모으고, <br className="m" />빌리고,<br className="pc" />불릴 수 있는<br className="m" /> 금융을 만들어요.</p>
-            </div>
-          </Box>
+    <Box boxClassName="content">
+      <div className="default_ani">
+        <Box boxClassName="inner">
+          <div className="titleWrap">
+            <p className="title1">돈을 모으고, <br className="m" />빌리고,<br className="pc" />불릴 수 있는<br className="m" /> 금융을 만들어요.</p>
+          </div>
+        </Box>
 
-          <Box boxClassName="inner w100">
-            <div className='imgWRap'>
-              <LazyLoadImage
-                alt=''                
-                effect="blur"
-                src={hero_img_pc}
-                className="pc-img" />
-              <LazyLoadImage
-                alt=''
-                effect="blur"                
-                src={hero_img_mw}
-                className="m-img" />
+        <Box boxClassName="inner w100">
+          <div className='imgWRap'>
+            <LazyLoadImage
+              alt=''
+              effect="blur"
+              src={bigBanner}
+              className={bigBannerClass} />
+          </div>
+        </Box>
+        <Box boxClassName="inner m">
+          <div className="textWrap">
+            <p className="text1">
+              케이뱅크는 구성원들이<br className="m" /> 성장할 다양한 기회와<br />
+              새로운 가능성을 끊임없이 제공합니다. <br />
+              <span className="pc">
+                이를 통해서 모든 구성원들은 치열하게 고민하고 과감하게 도전하며, <br />
+                우리가 가진 상상을 현실로 만들어가고 있습니다. <br />
+              </span>
+              케이뱅크는 무한한 가능성의 공간에서<br className="m" /> 모두가 꿈꾸는<br className="pc" />
+              금융권의 미래를<br className="m" /> 함께 만들어갈 여러분의 지원을 기다립니다.
+            </p>
+          </div>
+        </Box>
+        <Box boxClassName="inner pc">
+          <div className="textWrap">
+            <p className="text1">
+              케이뱅크는 구성원들이<br className="m" /> 성장할 다양한 기회와<br />
+              새로운 가능성을 끊임없이 제공합니다. <br />
+              <span className="pc">
+                이를 통해서 모든 구성원들은 치열하게 고민하고 과감하게 도전하며, <br />
+                우리가 가진 상상을 현실로 만들어가고 있습니다. <br />
+              </span>
+              케이뱅크는 무한한 가능성의 공간에서<br className="m" /> 모두가 꿈꾸는<br className="pc" />
+              금융권의 미래를<br className="m" /> 함께 만들어갈 여러분의 지원을 기다립니다.
+            </p>
+          </div>
+        </Box>
+      </div>
+
+      {/* 사람들 이야기 */}
+      <Box boxClassName="inner">
+        <div className='scroll-item'>
+          <div className="storyWrap">
+            <div className="inner-titleWrap">
+              <h2 className="title2">사람들 이야기</h2>
+              <Link to="/sub2" className="link">more</Link>
             </div>
-          </Box>
-          <Box boxClassName="inner m">
-            <div className="textWrap">
-              <p className="text1">
-                케이뱅크는 구성원들이<br className="m" /> 성장할 다양한 기회와<br />
-                새로운 가능성을 끊임없이 제공합니다. <br />
-                <span className="pc">
-                  이를 통해서 모든 구성원들은 치열하게 고민하고 과감하게 도전하며, <br />
-                  우리가 가진 상상을 현실로 만들어가고 있습니다. <br />
-                </span>
-                케이뱅크는 무한한 가능성의 공간에서<br className="m" /> 모두가 꿈꾸는<br className="pc" />
-                금융권의 미래를<br className="m" /> 함께 만들어갈 여러분의 지원을 기다립니다.
-              </p>
-            </div>
-          </Box>
-          <Box boxClassName="inner pc">
-            <div className="textWrap">
-              <p className="text1">
-                케이뱅크는 구성원들이<br className="m" /> 성장할 다양한 기회와<br />
-                새로운 가능성을 끊임없이 제공합니다. <br />
-                <span className="pc">
-                  이를 통해서 모든 구성원들은 치열하게 고민하고 과감하게 도전하며, <br />
-                  우리가 가진 상상을 현실로 만들어가고 있습니다. <br />
-                </span>
-                케이뱅크는 무한한 가능성의 공간에서<br className="m" /> 모두가 꿈꾸는<br className="pc" />
-                금융권의 미래를<br className="m" /> 함께 만들어갈 여러분의 지원을 기다립니다.
-              </p>
-            </div>
-          </Box>
+            <Swiper
+              pagination={{ clickable: true }}
+              modules={[Navigation, Pagination]}
+              lazyPreloadPrevNext={2}
+              breakpoints={{
+                768: {
+                  slidesPerView: 1,
+                  spaceBetween: 10,
+                  slidesPerGroup: 1,
+                },
+                801: {
+                  slidesPerView: 2,
+                  spaceBetween: 30,
+                  slidesPerGroup: 2,
+                }
+              }}
+              className="swiper-container swiper-container-horizontal mySwiper main-story"
+            >
+              {initialStoryCards.map((card, index) => (
+                <SwiperSlide className="swiper-slide" key={index}>
+                  <CardAction>
+                    {/* <CardMedia cardImgClassName="swiper-story-img" imageSrc={card.imageSrc}></CardMedia> */}
+                    <div className="img-item swiper-story-img">
+                      <img src={card.imageSrc} alt="" loading="lazy" />
+                    </div >
+                    <div className='swiper-story-txt1'>{card.title}</div>
+                    <div className='swiper-story-txt2'>{card.sub}</div>
+                  </CardAction>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
         </div>
-
-        {/* 사람들 이야기 */}
-        <Box boxClassName="inner">
-          <div className='scroll-item'>
-            <div className="storyWrap">
-              <div className="inner-titleWrap">
-                <h2 className="title2">사람들 이야기</h2>
-                <Link to="/sub2" className="link">more</Link>
-              </div>
-              <Swiper
-                pagination={{ clickable: true }}
-                modules={[Navigation, Pagination]}
-                breakpoints={{
-                  768: {
-                    slidesPerView: 1,
-                    spaceBetween: 10,
-                    slidesPerGroup: 1,
-                  },
-                  801: {
-                    slidesPerView: 2,
-                    spaceBetween: 30,
-                    slidesPerGroup: 2,
-                  }
-                }}
-                className="swiper-container swiper-container-horizontal mySwiper main-story"
-              >
-                {initialStoryCards.map((card, index) => (
-                  <SwiperSlide className="swiper-slide" key={index}>
-                    <CardAction>
-                      <CardMedia cardImgClassName="swiper-story-img" imageSrc={card.imageSrc}></CardMedia>
-                      <div className='swiper-story-txt1'>{card.title}</div>
-                      <div className='swiper-story-txt2'>{card.sub}</div>
-                    </CardAction>
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-            </div>
-          </div>
-        </Box>
-
-        {/* 케미코드  */}
-        <Box boxClassName="inner">
-        </Box>
-
-        {/* 베네핏 */}
-        <Box boxClassName="inner">
-          <div className="scroll-item" >
-            <div className="workbalanceWrap">
-              <div className="inner-titleWrap">
-                <h2 className="title2">일과 삶의 효율을 추구해요</h2>
-                <Link to="/sub3" className="link">more</Link>
-              </div>
-              <Swiper
-                pagination={true}
-                modules={[Pagination]}
-                breakpoints={{
-                  768: {
-                    slidesPerView: 1,
-                    spaceBetween: 10,
-                    slidesPerGroup: 1,
-                  },
-                  801: {
-                    slidesPerView: 2,
-                    spaceBetween: 30,
-                    slidesPerGroup: 2,
-                  },
-                  1000: {
-                    slidesPerView: 3,
-                    spaceBetween: 25,
-                    slidesPerGroup: 2,
-                  },
-                }}
-                className="swiper-container swiper-container-horizontal mySwiper main-workbalance"
-              >
-                {initialBenefitCard.map((card, index) => (
-                    <SwiperSlide className="swiper-slide" key={index}>
-                    <CardAction>
-                        <CardMedia cardImgClassName="swiper-workbalance-img" imageSrc={card.imageSrc}></CardMedia>
-                      <div className='swiper-workbalance-txt1'>{card.title}</div>
-                      <div className='swiper-workbalance-txt2'>{card.sub}</div>
-                    </CardAction>
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-            </div>
-          </div>
-        </Box>
-
-        {/* 공고바로가기 */}
-        <Box boxClassName="inner">
-          <div className="scroll-item">
-            <div className="recruit-linkWrap">
-              <img src={foot_img} alt="" />
-              <div className="txtWrap">
-                <p className="txt">지금 새로운 금융의 여정에<br className="m" /> 합류해 보세요.</p>
-                <Link to="/sub1" className="btn">채용공고 바로가기 →</Link>
-              </div>
-            </div>
-          </div>
-        </Box>
-
-        <Box boxClassName="inner">
-          <div className="snsWrap">
-            <p className="txt">케이뱅크의<br className="m" /> 다양한 소식을 확인해보세요.</p>
-            <ul>
-              <li><Link to="https://www.instagram.com/kbank_official/?hl=ko"><img src="images/ic_instargram.png" alt="instargram" /></Link></li>
-              <li><Link to="https://ko-kr.facebook.com/kbanknow"><img src="images/ic_facebook.png" alt="facebook" /></Link></li>
-              <li><Link to="https://www.youtube.com/@kbanknow"><img src="images/ic_youtube.png" alt="youtube" /></Link></li>
-            </ul>
-          </div>
-        </Box>
-
       </Box>
+
+      {/* 케미코드  */}
+      <Box boxClassName="inner">
+      </Box>
+
+      {/* 베네핏 */}
+      <Box boxClassName="inner">
+        <div className="scroll-item" >
+          <div className="workbalanceWrap">
+            <div className="inner-titleWrap">
+              <h2 className="title2">일과 삶의 효율을 추구해요</h2>
+              <Link to="/sub3" className="link">more</Link>
+            </div>
+            <Swiper
+              pagination={true}
+              modules={[Pagination]}
+              breakpoints={{
+                768: {
+                  slidesPerView: 1,
+                  spaceBetween: 10,
+                  slidesPerGroup: 1,
+                },
+                801: {
+                  slidesPerView: 2,
+                  spaceBetween: 30,
+                  slidesPerGroup: 2,
+                },
+                1000: {
+                  slidesPerView: 3,
+                  spaceBetween: 25,
+                  slidesPerGroup: 2,
+                },
+              }}
+              className="swiper-container swiper-container-horizontal mySwiper main-workbalance"
+            >
+              {initialBenefitCard.map((card, index) => (
+                <SwiperSlide className="swiper-slide" key={index}>
+                  <CardAction>
+                    <CardMedia cardImgClassName="swiper-workbalance-img" imageSrc={card.imageSrc}></CardMedia>
+                    <div className='swiper-workbalance-txt1'>{card.title}</div>
+                    <div className='swiper-workbalance-txt2'>{card.sub}</div>
+                  </CardAction>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </div>
+      </Box>
+
+      {/* 공고바로가기 */}
+      <Box boxClassName="inner">
+        <div className="scroll-item">
+          <div className="recruit-linkWrap">
+            <LazyImage src={footerImg} alt="" />
+            <div className="txtWrap">
+              <p className="txt">지금 새로운 금융의 여정에<br className="m" /> 합류해 보세요.</p>
+              <Link to="/sub1" className="btn">채용공고 바로가기 →</Link>
+            </div>
+          </div>
+        </div>
+      </Box>
+
+      <Box boxClassName="inner">
+        <div className="snsWrap">
+          <p className="txt">케이뱅크의<br className="m" /> 다양한 소식을 확인해보세요.</p>
+          <ul>
+            <li><Link to="https://www.instagram.com/kbank_official/?hl=ko"><img src="images/ic_instargram.png" alt="instargram" /></Link></li>
+            <li><Link to="https://ko-kr.facebook.com/kbanknow"><img src="images/ic_facebook.png" alt="facebook" /></Link></li>
+            <li><Link to="https://www.youtube.com/@kbanknow"><img src="images/ic_youtube.png" alt="youtube" /></Link></li>
+          </ul>
+        </div>
+      </Box>
+
+    </Box>
     // </ErrorBoundary>
   );
 }
